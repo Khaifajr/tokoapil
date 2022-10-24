53104366a1f4ec970680a4c98404759da4a74fdf
khaifajr <khaifajr01@gmail.com>
2022-10-24 20:50:46 +0700

    7

---
+++ app/Http/Controllers/ProdukController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produk;
+use Illuminate\Http\Request;
+
+class ProdukController extends Controllers
+{
+    public function create(Request $request)
+    {           
+        $kodeProduk = $request->input('kode_produk');
+        $namaProduk = $request->input('nama_produk');
+        $harga = $request->input('harga');
+
+        $produk = Produk::create([
+            'kode_produk' => $kodeProduk,
+            'nama_produk' => $namaProduk,
+            'harga' => $harga,
+        ]);
+        return $this->responseHasil(200,true, $produk);
+    }
+
+    public function list()
+    {
+        $produk = Produk::all();
+        return $this->responseHasil(200, true,$produk);
+    }
+
+    public function show($id)
+    {
+        $produk = Produk::findOrFail($id);
+        return $this->responseHasil(200, true, $produk);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $kodeProduk = $request->input('kode_produk'); 
+        $namaProduk = $request->input('nama_produk');
+        $harga = $request->input('harga');
+
+        $produk = Produk::findOrFail($id); 
+        $result = 
+        $produk->update([
+            'kode_produk' => $kodeProduk,
+            'nama_produk' => $namaProduk,
+            'harga' => $harga,
+        ]);
+        return $this->responseHasil(200,  true, $result);   
+    }
+
+    public function delete($id)
+    {
+        $produk = Produk::findOrFail($id);
+        $delete = $produk->delete();
+        return $this->responseHasil(200, true, $delete);
+    }
+}
---
+++ app/Models/Produk.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Produk extends Model
+{
+    protected $table = 'produk';
+
+    protected $fillable =['kode_produk', 'nama_produk', 'harga'];
+
+    public $timstamps = false;
+}
