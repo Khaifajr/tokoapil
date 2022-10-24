18e58392a6fef59d3ca09e25bcbaccac5b1dc9b9
khaifajr <khaifajr01@gmail.com>
2022-10-24 21:16:20 +0700

    7

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Produk;
 use Illuminate\Http\Request;
 
-class ProdukController extends Controllers
+class ProdukController extends Controller
 {
     public function create(Request $request)
     {           
